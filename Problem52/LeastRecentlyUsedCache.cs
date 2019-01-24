9bb3a69ab3f1915bc31268811b965620e25931d3
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-01-24 14:57:51 +0200

    Add skeleton for cache

---
+++ Problem52/LeastRecentlyUsedCache.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Problem52
+{
+	public class LeastRecentlyUsedCache
+	{
+		public LeastRecentlyUsedCache(int size)
+		{
+			Size = size;
+		}
+
+		public int Size { get; set; }
+
+		public void Set(string key, object value)
+		{
+
+		}
+
+		public void Get(string key)
+		{
+
+		}
+	}
+}
