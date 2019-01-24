1cb3dfcaa54ec994dcf66026777909a3080fab63
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-01-24 15:03:12 +0200

    Add test for get when element doesnt exist

---
+++ Tests/Problem52Tests/GetTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Problem52;
+
+namespace Tests.Problem52Tests
+{
+	[TestFixture]
+	public class GetTests
+	{
+		[TestCase("somekey")]
+		public void TestGet_ElementDoesNotExist_ShouldReturnNull(string key)
+		{
+			// Arrange
+			var cache = new LeastRecentlyUsedCache(10);
+
+			// Act
+			var result = cache.Get(key);
+
+			// Assert
+			Assert.IsNull(result);
+		}
+	}
+}
